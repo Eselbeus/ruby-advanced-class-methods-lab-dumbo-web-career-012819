fcb527a91812084aa0857de4cfc84ab556ba8f09
eselbeus <eselbeus@gmail.com>
2019-02-03 21:52:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -8,6 +11,29 @@ class Song
 
   def save
     self.class.all << self
+  end
+  
+  def self.create
+    @song = self.new
+    @name = @song.name
+    @@all << @song
+    # binding.pry
+    @song
+  end
+  
+  def self.new_by_name(name)
+    @song = Song.create
+    @name = @song.name
+    @@all << @song
+    # binding.pry
+  end
+  
+  def self.alphabetical
+    @@all.sort
+  end
+  
+  def self.destroy_all 
+    @@all.clear
   end
 
 end
